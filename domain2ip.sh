85080791d0d1b9b9bcbf96bb189654ef74611632
Sengshin Lee <sengshinlee@gmail.com>
2022-12-15 16:48:16 +0800

    Add a domain2ip.sh

---
+++ domain2ip.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function domain2ip() {
+    for A_DOMAIN in ${DOMAINS[*]}; do
+        AN_IPV4=$(dig ${A_DOMAIN} -4 +short | awk '{if($0!~"[a-z]")print}' | head -1)
+        echo "${AN_IPV4} ${A_DOMAIN}" | tee -a hosts
+    done
+}
+
+function main() {
+    DOMAINS=(
+        "api.github.com"
+        "atom.io"
+        "classroom.github.com"
+        "cli.github.com"
+        "codeload.github.com"
+        "desktop.github.com"
+        "docs.github.com"
+        "education.github.com"
+        "electronjs.org"
+        "gist.github.com"
+        "github.blog"
+        "github.com"
+        "github.community"
+        "github.dev"
+        "githubstatus.com"
+        "lab.github.com"
+        "octoverse.github.com"
+        "pages.github.com"
+        "partner.github.com"
+        "primer.style"
+        "raw.githubusercontent.com"
+        "resources.github.com"
+        "services.github.com"
+        "skills.github.com"
+        "socialimpact.github.com"
+        "support.github.com"
+        "thegithubshop.com"
+        "vscode.github.com"
+    )
+
+    if [ -f "hosts" ]; then
+        rm hosts
+    fi
+
+    domain2ip
+}
+
+main
